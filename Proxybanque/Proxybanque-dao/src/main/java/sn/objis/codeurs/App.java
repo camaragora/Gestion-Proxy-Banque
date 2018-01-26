d837f128a01ffbf71199c14174163b7f5e69c089
Gora Camara <courgora@gmail.com>
2018-01-26 02:26:52 +0000

    Création du projet multi modulaire maven et ajout des dépendances

---
+++ Proxybanque/Proxybanque-dao/src/main/java/sn/objis/codeurs/App.java
@@ -0,0 +1,13 @@
+package sn.objis.codeurs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
